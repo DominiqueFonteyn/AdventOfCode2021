fd1333b61962a92da8b11c0188f18329f2f0e93b
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-10 09:06:12 +0100

    failing test for Dive

---
+++ AdventOfCode.Tests/Day2/DiveTests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Day2;
+using Xunit;
+
+namespace AdventOfCode.Tests.Day2
+{
+    public class DiveTests
+    {
+        [Fact]
+        public void Run()
+        {
+            var instructions = new[]
+            {
+                "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"
+            };
+            
+            Assert.Equal(150, new Dive().Run(instructions));
+        }
+    }
+}
---
+++ AdventOfCode/Day2/Dive.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode.Day2
+{
+    public class Dive
+    {
+        public int Run(string[] instructions)
+        {
+            return 0;
+        }
+    }
+}

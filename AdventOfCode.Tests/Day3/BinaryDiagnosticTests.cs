961a0b58bca1caded417bdb1f974301f9d5b3a47
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-10 20:49:53 +0100

    day 3 - part 1

---
+++ AdventOfCode.Tests/Day3/BinaryDiagnosticTests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Day3;
+using Xunit;
+
+namespace AdventOfCode.Tests.Day3
+{
+    public class BinaryDiagnosticTests
+    {
+        private readonly string[] _input =
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        [Fact]
+        public void PartOne()
+        {
+            Assert.Equal(198, new BinaryDiagnostic().PartOne(_input));
+        }
+    }
+}
---
+++ AdventOfCode/Day3/BinaryDiagnostic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Day3
+{
+    public class BinaryDiagnostic
+    {
+        public decimal PartOne(string[] input)
+        {
+            var size = input[0].Length;
+            var gammaArray = new int[size];
+            var epsilonArray = new int[size];
+
+            for (var i = 0; i < size; i++)
+            {
+                gammaArray[i] = MostCommonBit(input, i);
+                epsilonArray[i] = Math.Abs(gammaArray[i] - 1);
+            }
+
+            var gamma = BitArrayToInt(gammaArray);
+            var epsilonRate = BitArrayToInt(epsilonArray);
+
+            return gamma * epsilonRate;
+        }
+
+        public decimal PartTwo(string[] input)
+        {
+            return 0;
+        }
+
+        private int MostCommonBit(string[] input, int position)
+        {
+            var total = input.Sum(x => int.Parse(x[position].ToString()));
+            if (total > input.Length / 2) return 1;
+            return 0;
+        }
+
+        private int BitArrayToInt(int[] bits)
+        {
+            return Convert.ToInt32(string.Join("", bits), 2);
+        }
+    }
+}

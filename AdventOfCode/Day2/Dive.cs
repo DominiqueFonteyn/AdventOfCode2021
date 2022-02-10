08a2540d46de694daf56d40918352361cdb57248
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-10 09:13:35 +0100

    pass test

--- AdventOfCode/Day2/Dive.cs
+++ AdventOfCode/Day2/Dive.cs
@@ -4,7 +4,30 @@
     {
         public int Run(string[] instructions)
         {
-            return 0;
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var instruction in instructions)
+            {
+                var split = instruction.Split(new[] { ' ' });
+                var direction = split[0];
+                var amount = int.Parse(split[1]);
+
+                switch (direction)
+                {
+                    case "forward":
+                        horizontal += amount;
+                        break;
+                    case "up":
+                        depth -= amount;
+                        break;
+                    case "down":
+                        depth += amount;
+                        break;
+                }
+            }
+
+            return horizontal * depth;
         }
     }
 }

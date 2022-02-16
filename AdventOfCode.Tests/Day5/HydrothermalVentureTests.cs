32cf5eac46af8d526cb907db8d09961546837c81
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-16 12:19:34 +0100

    coordinate

---
+++ AdventOfCode.Tests/Day5/HydrothermalVentureTests.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Day5;
+using Xunit;
+
+namespace AdventOfCode.Tests.Day5
+{
+    public class HydrothermalVentureTests
+    {
+        private readonly string[] _input =
+        {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2"
+        };
+
+        [Fact]
+        public void PartOne()
+        {
+            Assert.Equal(5, new HydrothermalVenture().PartOne(_input));
+        }
+    }
+
+    public class GridTests
+    {
+    }
+
+    public class VectorTests
+    {
+    }
+
+    public class CoordinateTests
+    {
+        private const int Y = 9;
+        private const int X = 5;
+
+        [Fact]
+        public void CtorSetsX()
+        {
+            Assert.Equal(X, new Coordinate(X, Y).X);
+        }
+
+        [Fact]
+        public void CtorSetsY()
+        {
+            Assert.Equal(Y, new Coordinate(X, Y).Y);
+        }
+
+        [Fact]
+        public void OverridesToString()
+        {
+            Assert.Equal("5,9", new Coordinate(X,Y).ToString());
+        }
+    }
+}
---
+++ AdventOfCode/Day5/HydrothermalVenture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AdventOfCode.Day5
+{
+    public class HydrothermalVenture : Day<int>
+    {
+        public HydrothermalVenture() : base(5)
+        {
+        }
+
+        public override int PartOne(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int PartTwo(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Grid
+    {
+        private readonly int _height;
+        private readonly int _width;
+
+        private int[,] _grid;
+
+        public Grid(int width, int height)
+        {
+            _width = width;
+            _height = height;
+
+            _grid = new int[height, width];
+        }
+    }
+
+    public class Vector
+    {
+        public Vector(Coordinate start, Coordinate end)
+        {
+            
+        }
+    }
+
+    public class Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+    }
+}

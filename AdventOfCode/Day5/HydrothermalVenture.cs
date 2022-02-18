104042152ceb696f080de95abc0a987e7b2b2b68
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-18 16:31:11 +0100

    dynamic grid size

--- AdventOfCode/Day5/HydrothermalVenture.cs
+++ AdventOfCode/Day5/HydrothermalVenture.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Day5
 {
@@ -10,18 +12,28 @@ namespace AdventOfCode.Day5
 
         public override int PartOne(string[] input)
         {
-            var grid = new Grid(10, 10);
-
+            var vectors = new List<Vector>();
+            var width = 0;
+            var height = 0;
             foreach (var line in input)
             {
                 var vector = Vector.FromString(line);
-                if (vector.Direction == VectorDirection.Other)
-                    continue;
+                vectors.Add(vector);
 
+                var w = Math.Max(vector.Start.X, vector.End.X);
+                if (width < w)
+                    width = w;
+                var h = Math.Max(vector.Start.Y, vector.End.Y);
+                if (height < h)
+                    height = h;
+            }
+
+            var grid = new Grid(height + 1, width + 1);
+
+            foreach (var vector in vectors.Where(vector => vector.Direction != VectorDirection.Other))
+            {
                 Console.WriteLine($"Applying {vector}");
-                grid.Apply(Vector.FromString(line));
-                Console.WriteLine(grid);
-                Console.WriteLine();
+                grid.Apply(vector);
             }
 
             return grid.CountOverlaps();

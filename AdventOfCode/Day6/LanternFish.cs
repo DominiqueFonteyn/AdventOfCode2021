eca8fe2151e21fbd0043821d06c3c09d37d5c122
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-02-18 21:32:24 +0100

    day 6 part one

--- AdventOfCode/Day6/LanternFish.cs
+++ AdventOfCode/Day6/LanternFish.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode.Day6
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day6
 {
     public class LanternFish : Day<int>
     {
@@ -8,12 +12,39 @@
 
         public override int PartOne(string[] input)
         {
-            throw new System.NotImplementedException();
+            var fish = new List<int>(input[0].Split(',').Select(int.Parse));
+
+            Iterate(fish, 80);
+
+            return fish.Count;
         }
 
         public override int PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        public void Iterate(List<int> fish, int numberOfDays)
+        {
+            for (var i = 0; i < numberOfDays; i++)
+            {
+                var spawn = 0;
+                for (var f = 0; f < fish.Count; f++)
+                {
+                    if (fish[f] == 0)
+                    {
+                        spawn++;
+                        fish[f] = 6;
+                    }
+                    else
+                    {
+                        fish[f] -= 1;
+                    }
+                }
+
+                for (var j = 0; j < spawn; j++) 
+                    fish.Add(8);
+            }
         }
     }
 }

a2abdd4e7a2927206b54cd3a12c743258e5c5f1a
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-03-03 12:24:17 +0100

    setup for day 9

---
+++ AdventOfCode.Tests/Day9/SmokeBasinTests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Day9;
+using Xunit;
+
+namespace AdventOfCode.Tests.Day9
+{
+    public class SmokeBasinTests
+    {
+        private readonly string[] _input =
+        {
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678"
+        };
+
+        [Fact]
+        public void PartOne()
+        {
+            Assert.Equal(15, new SmokeBasin().PartOne(_input));
+        }
+    }
+}
---
+++ AdventOfCode/Day9/SmokeBasin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdventOfCode.Day9
+{
+    public class SmokeBasin : Day<int>
+    {
+        public SmokeBasin() : base(9)
+        {
+        }
+
+        public override int PartOne(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int PartTwo(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

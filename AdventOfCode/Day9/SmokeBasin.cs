cf99dbcb0527a67e267eb2069cfa5a7ffd4f0349
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-03-04 12:29:54 +0100

    separate file

--- AdventOfCode/Day9/SmokeBasin.cs
+++ AdventOfCode/Day9/SmokeBasin.cs
@@ -77,43 +77,4 @@ namespace AdventOfCode.Day9
             yield return new[] { 0, 1 };
         }
     }
-
-    public class Point
-    {
-        public Point(int i, int j, int value)
-        {
-            I = i;
-            J = j;
-            Value = value;
-        }
-
-        public int I { get; }
-        public int J { get; }
-        public int Value { get; }
-
-        public override bool Equals(object? obj)
-        {
-            return Equals(obj as Point);
-        }
-
-        private bool Equals(Point other)
-        {
-            return I == other.I && J == other.J && Value == other.Value;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(I, J, Value);
-        }
-
-        public static bool operator ==(Point left, Point right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Point left, Point right)
-        {
-            return !Equals(left, right);
-        }
-    }
 }

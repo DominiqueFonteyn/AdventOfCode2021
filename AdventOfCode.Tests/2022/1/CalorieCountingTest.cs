5a91a3a6b8d8fc1bc7a18bb5548a3131ce180062
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-12-01 21:57:30 +0100

    [] PHOENIX 2022 day 1

---
+++ AdventOfCode.Tests/2022/1/CalorieCountingTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Tests._2022._1
+{
+    public class CalorieCountingTest
+    {
+        [Fact]
+        public void Part1_Example()
+        {
+            var mostCalories = new CalorieCounting().FindMostCalories(File.ReadAllLines("2022/1/example.txt"));
+            Assert.Equal(24000, mostCalories);
+        }
+
+        [Fact]
+        public void Part1_Input()
+        {
+            var input = File.ReadAllLines("2022/1/input.txt");
+            Assert.Equal(0, new CalorieCounting().FindMostCalories(input));
+        }
+
+        [Fact]
+        public void Part2_Example()
+        {
+            var mostCalories = new CalorieCounting().FindMostCalories(File.ReadAllLines("2022/1/example.txt"));
+            Assert.Equal(45000, mostCalories);
+        }
+
+        [Fact]
+        public void Part2_Input()
+        {
+            var input = File.ReadAllLines("2022/1/input.txt");
+            Assert.Equal(0, new CalorieCounting().FindMostCalories(input));
+        }
+    }
+
+    public class CalorieCounting
+    {
+        public int FindMostCalories(string[] input)
+        {
+            var calories = new List<int> { 0 };
+
+            foreach (var value in input)
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    calories.Add(0);
+                    continue;
+                }
+
+                calories[^1] += int.Parse(value);
+            }
+
+            return calories
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum(x => x);
+        }
+    }
+}

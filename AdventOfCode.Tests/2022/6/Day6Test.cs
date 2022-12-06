19bf07f9c017cf38464ddf10a8b2bac5aab3626e
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-12-06 20:58:13 +0100

    [] PHOENIX day 6 part 2

--- AdventOfCode.Tests/2022/6/Day6Test.cs
+++ AdventOfCode.Tests/2022/6/Day6Test.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -12,7 +11,7 @@ namespace AdventOfCode.Tests._2022._6
         }
 
         protected override int ExpectedResultPart1 => 7;
-        protected override int ExpectedResultPart2 { get; }
+        protected override int ExpectedResultPart2 => 19;
 
         protected override int Calculate(string[] data)
         {
@@ -32,7 +31,18 @@ namespace AdventOfCode.Tests._2022._6
 
         protected override int Calculate2(string[] data)
         {
-            throw new NotImplementedException();
+            var input = data.Single();
+            for (var i = 0; i < input.Length - 14; i++)
+            {
+                var fragment = input.Substring(i, 14);
+                var set = new HashSet<char>(fragment);
+                if (set.Count == 14)
+                {
+                    return i + 14;
+                }
+            }
+
+            return 0;
         }
 
         [Theory]
@@ -44,6 +54,31 @@ namespace AdventOfCode.Tests._2022._6
         public void TestCalculate(string input, int expected)
         {
             Assert.Equal(expected, Calculate(new[] { input }));
+        }
+
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void TestCalculate2(string input, int expected)
+        {
+            // var result = 0;
+            // for (var i = 0; i < input.Length - 14; i++)
+            // {
+            //     var fragment = input.Substring(i, 14);
+            //     var set = new HashSet<char>(fragment);
+            //     
+            //     if (set.Count == 14)
+            //     {
+            //         result = i;
+            //         break;
+            //     }
+            // }
+            // Assert.Equal(expected, result);
+
+            Assert.Equal(expected, Calculate2(new[] { input }));
         }
     }
 }

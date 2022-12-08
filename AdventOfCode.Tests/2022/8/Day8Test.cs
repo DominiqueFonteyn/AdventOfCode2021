5f04855e30fea00fcc40aa9626d0f7f771847aaf
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-12-08 11:51:21 +0100

    [] PHOENIX day 8 part 1

---
+++ AdventOfCode.Tests/2022/8/Day8Test.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode.Tests._2022._8
+{
+    public class Day8Test : TestBase
+    {
+        public Day8Test() : base(2022, 8)
+        {
+        }
+
+        protected override int ExpectedResultPart1 => 21;
+        protected override int ExpectedResultPart2 => 24933642;
+
+        protected override int Calculate(string[] data)
+        {
+            var rows = data.Length;
+            var cols = data[0].Length;
+            var grid = InitGrid(data, rows, cols);
+
+            var visibleTrees = 0;
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+            {
+                if (IsEdge(row, rows, col, cols))
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                var left = true;
+                for (var i = col - 1; i >= 0 && left; i--) left &= grid[row, i] < grid[row, col];
+                if (left)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                var right = true;
+                for (var i = col + 1; i < cols && right; i++) right &= grid[row, i] < grid[row, col];
+                if (right)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+                
+                var top = true;
+                for (var i = row - 1; i >= 0 && top; i--) top &= grid[i, col] < grid[row, col];
+                if (top)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+                
+                var bottom = true;
+                for (var i = row + 1; i < rows && bottom; i++) bottom &= grid[i, col] < grid[row, col];
+                if (bottom)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+            }
+
+            return visibleTrees;
+        }
+
+        private static bool IsEdge(int row, int rows, int col, int cols)
+        {
+
+            return row == 0 || row == rows - 1 || col == 0 || col == cols - 1;
+        }
+
+        private int[,] InitGrid(string[] data, int rows, int cols)
+        {
+            var grid = new int[rows, cols];
+
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < cols; col++)
+                grid[row, col] = int.Parse($"{data[row][col]}");
+
+            return grid;
+        }
+
+        protected override int Calculate2(string[] data)
+        {
+            return 0;
+        }
+    }
+}
